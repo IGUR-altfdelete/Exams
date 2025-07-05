e04675cdc81df23c2cc7fd683a4165246cfbdd16
IGUR-altfdelete <gurido15@gmail.com>
2025-07-05 09:45:10 +0200

    Update ft_strcspn.c

--- EXAM02/level2/ft_strcspn.c
+++ EXAM02/level2/ft_strcspn.c
@@ -18,14 +18,16 @@ size_t	ft_strcspn(const char *s, const char *reject)
 	size_t	j;
 
 	i = 0;
-	while (s[i++])
+	while (s[i])
 	{
 		j = 0;
 		while (reject[j])
 		{
-			if (s[i] == reject[j++])
+			if (s[i] == reject[j])
 				return (i);
+			j++;
 		}
+		i++;
 	}
 	return (i);
 }
