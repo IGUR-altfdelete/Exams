41e1a802d5c39d862880e35256a5e6d7c100536d
IGUR-altfdelete <gurido15@gmail.com>
2025-07-06 20:38:31 +0200

    Update last_word.c

--- EXAM02/level2/last_word.c
+++ EXAM02/level2/last_word.c
@@ -9,27 +9,25 @@
 /*   Updated: 2025/06/07 16:22:32 by igur             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	int	i;
-
-	i = 0;
 	if (ac == 2)
 	{
+		int i = 0;
 		while (av[1][i])
 			i++;
 		i--;
-		while (av[1][i] < 32)
+		while (i >= 0 && (av[1][i] == ' ' || av[1][i] == '\t'))
+			i--;
+		int end = i;
+		while (i >= 0 && av[1][i] > 32)
 			i--;
 		i++;
-		while (av[1][i])
-		{
-			write(1, &av[1][i], 1);
-			i++;
-		}
+		while (i <= end)
+			write(1, &av[1][i++], 1);
 	}
 	write(1, "\n", 1);
 }
+
